c6891a1b107508ed8dad64e696a505eeba05cd38
Abhishek Upadhyay <megablazikenabhishek@gmail.com>
2023-08-21 21:30:52 +0530

    done

---
+++ admin/src/pages/AdminHome.tsx
@@ -0,0 +1,13 @@
+import NavBar from '../components/AdminNavBar'
+
+export default function Home() {
+
+    return (
+        <>
+            <NavBar />
+            <div>
+                Hello, World !!
+            </div>
+        </>
+    )
+}
