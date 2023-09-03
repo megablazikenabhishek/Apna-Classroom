5f8e839a65c2560a6700afe2f53eabd879348049
ikachu-7 <vivek.a.tiwari13@gmail.com>
2023-09-03 20:56:14 +0530

    front pge

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import HomeScreen from "./pages/Home";
 import Room from "./pages/Room";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./pages/AuthPages/Sign-up/SignUp";
+import "./App.css";
+import Calls from "./components/calls/index";
+import Test from "./Test";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Route path="/room" element={<Room />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/home" element={<Calls />} />
+        <Route path="/activity" element={<HomeScreen />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </BrowserRouter>
   );
